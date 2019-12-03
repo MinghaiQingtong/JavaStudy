6abb2392aea43a8240eaf782faa56ce256384f87
Tang Tong <minghaiqingtong@163.com>
2019-12-03 15:10:48 +0800

    test

--- src/helloworld/h1.java
+++ src/helloworld/h1.java
@@ -4,7 +4,7 @@ public class h1 {
 	public static void main(String []args) {
 		
 		System.out.println("Hello World!");
-		//
+		////
 		int N = 100000;
 		long start , end;
 		start = System.currentTimeMillis();
